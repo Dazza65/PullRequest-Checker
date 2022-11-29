8c56bdf0cf9510e16388600bc8cab08bbd41e95a
Darren Harris <dazza@me.com>
2022-11-29 20:42:38 +0800

    feat: add codecommit repo

--- lib/pull_request-checker-stack.ts
+++ lib/pull_request-checker-stack.ts
@@ -1,16 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Repository } from 'aws-cdk-lib/aws-codecommit';
 
 export class PullRequestCheckerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const codeRepository = Repository.fromRepositoryName(this, 'CodeRepository', 'prj1ex');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PullRequestCheckerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
