b9682c32f349adf64c291326f8b60c79ce8a7e51
Darren Harris <dazza@me.com>
2022-11-29 21:06:53 +0800

    feat: add codebuild project for pull request

--- lib/pull_request-checker-stack.ts
+++ lib/pull_request-checker-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Repository } from 'aws-cdk-lib/aws-codecommit';
+import { Project, ComputeType, LinuxArmBuildImage, Source } from 'aws-cdk-lib/aws-codebuild';
 
 export class PullRequestCheckerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,15 @@ export class PullRequestCheckerStack extends cdk.Stack {
 
     const codeRepository = Repository.fromRepositoryName(this, 'CodeRepository', 'prj1ex');
 
-    
+    const pullRequestCodeBuildProject = new Project(this, 'PRCodeBuildPrj', {
+      environment: {
+        computeType: ComputeType.SMALL,
+        buildImage: LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_2_0
+      },
+      projectName: 'prj1ex-pr',
+      source: Source.codeCommit({
+        repository: codeRepository
+      })
+    });
   }
 }
