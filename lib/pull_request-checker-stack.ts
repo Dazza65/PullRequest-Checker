9ea5d34364ed281ea5e00a86041ae82be068b93c
Darren Harris <dazza@me.com>
2022-11-29 21:57:59 +0800

    feat: add onPullRequestStateChange event on the code repo to trigger CodeBuild

--- lib/pull_request-checker-stack.ts
+++ lib/pull_request-checker-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Repository } from 'aws-cdk-lib/aws-codecommit';
 import { Project, ComputeType, LinuxArmBuildImage, Source } from 'aws-cdk-lib/aws-codebuild';
+import { CodeBuildProject } from 'aws-cdk-lib/aws-events-targets';
 
 export class PullRequestCheckerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,9 +16,23 @@ export class PullRequestCheckerStack extends cdk.Stack {
         buildImage: LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_2_0
       },
       projectName: 'prj1ex-pr',
+      description: 'Builds the project triggered by the CodeCommit Pull Request',
       source: Source.codeCommit({
         repository: codeRepository
       })
+    });
+
+    codeRepository.onPullRequestStateChange('pullRequestStatusChange', {
+      description: 'Triggers the pullRequestCodeBuildProject on a pull request',
+      ruleName: 'prStatusChange',
+      eventPattern: {
+        detailType: ['CodeCommit Pull Request State Change'],
+        source: ['aws.codecommit'],
+        detail: {
+          'event': ['pullRequestCreated', 'pullRequestSourceBranchUpdated']
+        }
+      },
+      target: new CodeBuildProject(pullRequestCodeBuildProject)
     });
   }
 }
