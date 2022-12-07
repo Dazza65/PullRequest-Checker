1e2e65f435ec26d0dced2542d774bac0289d6b71
Darren Harris <dazza@me.com>
2022-12-07 16:49:14 +0800

    chore: restructure project

---
+++ test/pull-request-checker.test.ts
@@ -0,0 +1,16 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import { PullRequestChecker } from '../src/pull-request-checker';
+
+
+test('PullRequestChecker has a CodeBuild project with the specified name', () => {
+  const stack = new Stack(new App());
+  
+  new PullRequestChecker(stack, 'MyTestStack', { repositoryName: 'testrepo' });
+  
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::CodeBuild::Project', {
+    Name: 'PullRequestChecker-testrepo'
+ });
+});
